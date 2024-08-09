e7c6c6343676e0cc1268a8637b5e0b6052ab214f
Matrixx112 <chiemekaonwunkwe@gmail.com>
2024-08-09 12:50:48 +0300

    index.yuji

---
+++ interface.js
@@ -0,0 +1,26 @@
+// let text = document.getElementById("first")
+// console.log(text)
+let y=document.getElementsByClassName("second")
+console.log(y)
+// let g=document.querySelector(".first")
+// console.log(g)
+let h=document.querySelectorAll("#cg")
+console.log(h)
+cg.style.color="blue"
+cg.style.backgroundColor="red"
+cg.style.fontSize="100px"
+cg.innerHTML ="moses"
+let d=document.querySelector(".clear")
+d.innerHTML = "ujyjyhtbrgty7nhyrf"
+let bu=document.querySelector("butt")
+function m(){
+    console.log("my name")
+cg.innerHTML ="new"
+
+}
+function g(){
+    console.log(
+"nigeria"
+    )
+    cg.innerHTML="food"
+}
